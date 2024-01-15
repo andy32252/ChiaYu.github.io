e9e43260142854aef36f716610ad9f2932cf18c2
Chia-Yu <andy32252@gmail.com>
2024-01-15 19:40:43 +0800

    update

--- assets2/js/index.js
+++ assets2/js/index.js
@@ -5,19 +5,15 @@ $(this).bind("contextmenu", function(e) {
         e.preventDefault();
     });
 });
-/*
-var firebaseConfig = {
-    apiKey: "AIzaSyCIzZvf736MqyBS_zCymZH5WIpeipGyCvU",
-    authDomain: "myfirstpage-7adfc.firebaseapp.com",
-    databaseURL: "https://myfirstpage-7adfc-default-rtdb.firebaseio.com",
-    projectId: "myfirstpage-7adfc",
-    storageBucket: "myfirstpage-7adfc.appspot.com",
-    messagingSenderId: "408570385036",
-    appId: "1:408570385036:web:2efd335127cdee7b0061a9",
-    measurementId: "G-0M0B315SPN"
-  };
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-var db = firebase.firestore();
-*/
+const firebaseConfig = {
+  apiKey: "AIzaSyAOncDxiVdxErSR8IGy2bOKyBYcKJ2o_7E",
+  authDomain: "pagetest-20568.firebaseapp.com",
+  projectId: "pagetest-20568",
+  storageBucket: "pagetest-20568.appspot.com",
+  messagingSenderId: "813105388025",
+  appId: "1:813105388025:web:0cd0a15a8e2676883a0528",
+  measurementId: "G-WFVHF0FV22"
+};
+
+
